534508d92afb1e0b713f0a3d3afafa1a07802b3e
bobrikilya <ilya.bobrik.2020@gmail.com>
2020-11-23 22:38:00 +0300

    Create file reader class

---
+++ file_reader.py
@@ -0,0 +1,25 @@
+import json
+
+class FileReader:
+    """Class for read the file that was transferred """
+
+    def file_reader(self, file):
+        try:
+            with open(file) as read_file:
+                data = json.load(read_file)
+            return data
+
+        except FileNotFoundError:
+            print("Your file not founding")
+
+
+
+
+
+
+
+
+
+
+
+
